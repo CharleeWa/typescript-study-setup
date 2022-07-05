d4a78e98f42c48e48bff5ef36e06d1b2ccc1ee6c
wangchao <18888351756@163.com>
2022-07-05 14:48:48 +0800

    chore: 学习 原始类型与对象类型标注

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
+// 数据类型标注
+const user: string = 'linbudu'
+const age: number = 24
+const male: boolean = false
+const undef: undefined = undefined
+const nul: null = null
+const obj: object = { user, age, male }
+const bigintVar1: bigint = 9007199254740991n
+const bigintVar2: bigint = BigInt(9007199254740991)
+const symbolVar: symbol = Symbol('unique')
+const arr1: string[] = []
+const arr2: Array<string> = []
+
+interface IDescription {
+  readonly name: string;
+  age: number;
+  male?: boolean;
+  func?: Function
+}
+
+const obj1: IDescription = {
+  name: 'cwang',
+  age: 100,
+  male: true,
+}
+
 console.log('Hello TypeScript')
+console.log(obj1)
